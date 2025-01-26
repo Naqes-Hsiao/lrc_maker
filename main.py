ff3306bb89008fb3da8d9accff1b253d5dabaec5
Naqes Hsiao <littlepick09@gmail.com>
2025-01-26 14:29:36 +0800

    modify first

---
+++ main.py
@@ -0,0 +1,8 @@
+from interface_manager import InterfaceManager
+import tkinter as tk
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    interface_manager = InterfaceManager(window)
+    window.mainloop()
+    print("hello world")
