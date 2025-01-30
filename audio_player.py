5ca1cb02b0e8da2ec0af9bcbc001275d3abb81ea
Naqes Hsiao <littlepick09@gmail.com>
2025-01-30 21:43:43 +0800

    modify audio_player second

--- audio_player.py
+++ audio_player.py
@@ -29,17 +29,17 @@ class AudioPlayer:
 
     def _play(self):
         self.__audio = self.__audio.set_sample_width(2)
+        sample = np.array(self.__audio.get_array_of_samples()).reshape(-1, 2)
         stream = self.__p.open(
             format=self.__p.get_format_from_width(self.__audio.sample_width),
             channels=self.__audio.channels,
             rate=self.__audio.frame_rate,
             output=True
         )
-        sample = np.array(self.__audio.get_array_of_samples()).reshape(-1, 2)
-        stream.write(sample)
+        stream.write(sample.tobytes())
 
     def play(self):
-        self.__thread = threading.Thread(target=self._play)
+        self.__thread = threading.Thread(target=self._play, daemon=True)
         self.__thread.start()
 
     def pause(self):
