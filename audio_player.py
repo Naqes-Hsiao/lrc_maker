7db7b7a9572ad23b3629a3f45f1c14c0b277ee29
Naqes Hsiao <littlepick09@gmail.com>
2025-01-30 21:18:48 +0800

    modify audio_player first

--- audio_player.py
+++ audio_player.py
@@ -1,79 +1,46 @@
 import tkinter.filedialog as tkfd
-import pygame as pg
+from pydub import AudioSegment
+import pyaudio
+import threading
+import numpy as np
 
 
 class AudioPlayer:
     def __init__(self):
         self.__file_path = None
-        self.__is_play = False
-        self.__file_length = 0
-        self.__difference = 0
-        self.__is_first_play = True
+        self.__audio = None
+        self.__thread = None
 
-        pg.mixer.init()
-
-    def get_file_length(self):
-        return self.__file_length
+        self.__p = pyaudio.PyAudio()
 
     def load(self):
-        self.__file_path = tkfd.askopenfilename(filetypes=[("音频文件", "*.mp3; *.flac")])
+        self.__file_path = tkfd.askopenfilename(filetypes=[("音频文件", "*.flac;*.mp3")])
         if self.__file_path:
-            pg.mixer.music.load(self.__file_path)
-            self.__file_length = pg.mixer.Sound(self.__file_path).get_length()
+            self.__audio = AudioSegment.from_file(self.__file_path)
         return self.__file_path
 
     def reload(self):
         temp_file_path = self.__file_path
-        self.__file_path = tkfd.askopenfilename(filetypes=[("音频文件", "*.mp3; *.flac")])
+        self.__file_path = tkfd.askopenfilename(filetypes=[("音频文件", "*.flac;*.mp3")])
         if not self.__file_path:
             self.__file_path = temp_file_path
         else:
-            pg.mixer.music.stop()
-            pg.mixer.music.unload()
-            pg.mixer.music.load(self.__file_path)
-            self.__file_length = pg.mixer.Sound(self.__file_path).get_length()
+            self.__audio = AudioSegment.from_file(self.__file_path)
+
+    def _play(self):
+        self.__audio = self.__audio.set_sample_width(2)
+        stream = self.__p.open(
+            format=self.__p.get_format_from_width(self.__audio.sample_width),
+            channels=self.__audio.channels,
+            rate=self.__audio.frame_rate,
+            output=True
+        )
+        sample = np.array(self.__audio.get_array_of_samples()).reshape(-1, 2)
+        stream.write(sample)
 
     def play(self):
-        if self.__is_first_play:
-            pg.mixer.music.play()
-            self.__is_first_play = False
-        else:
-            pg.mixer.music.unpause()
-        self.__is_play = True
+        self.__thread = threading.Thread(target=self._play)
+        self.__thread.start()
 
     def pause(self):
-        self.play()
-        pg.mixer.music.pause()
-        self.__is_play = False
-
-    def restart(self):
-        is_over = self.__is_play and not pg.mixer.music.get_busy()
-        if is_over:
-            pg.mixer.music.stop()
-            self.__is_first_play = True
-            self.__is_play = False
-            self.__difference = 0
-        return is_over
-
-    def set_position(self, position):
-        self.pause()
-        target_time = position / 100 * self.__file_length
-        pg.mixer.music.set_pos(target_time)
-        self.__difference = target_time - pg.mixer.music.get_pos() / 1000
-
-    def get_position(self):
-        if self.__is_play:
-            current_time = pg.mixer.music.get_pos() / 1000 + self.__difference
-            position = current_time / self.__file_length * 100
-            return position
-        else:
-            return 0
-
-    def reset(self):
-        pg.mixer.music.stop()
-        pg.mixer.music.unload()
-        self.__file_path = None
-        self.__is_play = False
-        self.__file_length = 0
-        self.__difference = 0
-        self.__is_first_play = True
+        pass
